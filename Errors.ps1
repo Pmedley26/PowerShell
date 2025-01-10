081ec563b7cf80e3dd23d0757934ce00e236f0ad
Patrick Medley <smcinnism@gmail.com>
2025-01-10 00:32:06 -0500

    Add files via upload
    Conceptual Error Action Script

---
+++ Errors.ps1
@@ -0,0 +1,11 @@
+﻿$folderpath = '.\bogusFolder'
+try {
+$files = Get-Childitem -Path $folderPath -ErrorAction Stop
+Write-Host "This shouldn't run."
+$files.foreach({
+$fileText = Get-Content $files
+$fileText[0]
+})
+} catch {
+$_.Exception.Message
+}

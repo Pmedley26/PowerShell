d8fa837c2a81c0c7888e65775382641c98d0a7da
Patrick Medley <smcinnism@gmail.com>
2025-01-15 02:46:26 -0500

    Add files via upload
    Same as install-software file, with process block added to demonstrate pushing the function into the pipeline for all objects within the scipt

---
+++ process.ps1
@@ -0,0 +1,25 @@
+﻿function Install-Software {
+[Cmdletbinding()]
+param(
+[parameter(Mandatory)]
+[ValidateSet('1','2')] 
+[string]$Version ,
+
+[parameter(Mandatory , ValuefromPipeline)]
+[string]$Computername 
+
+) 
+
+process {
+Write-host "I installed software version $Version on $Computername."
+}
+
+
+}
+
+$computers = @("SRV1", "SRV2", "SRV3")
+$computers | Install-Software -Version 2
+
+
+
+

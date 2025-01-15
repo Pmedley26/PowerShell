66e05c6aa77bebc41eb2004b2bed7ae1ce7b3f24
Patrick Medley <smcinnism@gmail.com>
2025-01-15 02:44:37 -0500

    Add files via upload
    Practice with creating a function and adding attributes to a parameter block to modify a -ComputerName Script

---
+++ Install.software.ps1
@@ -0,0 +1,21 @@
+﻿function Install-Software {
+[Cmdletbinding()]
+param(
+[parameter(Mandatory)]
+[ValidateSet('1','2')]
+[string]$Version ,
+
+[Parameter(Mandatory, ValueFromPipeline)]
+[string]$ComputerName
+)
+
+# A default value can be added for the parameter block. The above shows version 2 for the default
+# By adding the Mandatory attribute with the parameter block, you requie a version to be specified for the function to work. 
+Write-host "I installed software version $Version on $ComputerName"
+}
+
+$computers = @("SRV1", "SRV2", "SRV3")
+foreach ($pc in $computers) {
+
+Install-Software version 2 -ComputerName $pc
+}

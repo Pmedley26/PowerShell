57e0af3560d293319b4d773e16c16eb6cfc3b88f
Patrick Medley <smcinnism@gmail.com>
2025-02-11 15:17:21 -0800

    Inventory.ps1

--- Inventory.ps1
+++ Inventory.ps1
@@ -1,5 +1,7 @@
 ﻿$rows = Import-Csv -path C:\Users\Administrator\Documents\IPAddresses.csv
 
+
+#Create Hashtable specifying IP, Department, Is the IP Reachable, What is hostname, and How to handle Errors. 
 foreach($row in $rows) {
 try {
 $output = @{
@@ -10,15 +12,19 @@ HostName = $null
 Error = $null
 } 
 
+#Ping IP address from csv once, if reachable, set hashtable value to true
 if (Test-connection -ComputerName $row.IPAddress -Count 1 -Quiet) {
 $output.IsOnline = $true
 }
 
+#Check if Hostname can be found, and update hashtable value if found. 
 if ($hostname = (Resolve-DnsName -Name $row.IPAddress -ErrorAction Stop).Name) {
 $output.Hostname = $hostName
 }
 } catch {
 $output.Error = $_.Exception.Message
+
+#Create a PScustomobject to which the output of the script will save, allowing the CSV to be exported
 } finally {
 [pscustomobject]$output
 }

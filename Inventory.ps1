a24e58bfbf32a3f3a54c3a3b0737f78800cc560e
Patrick Medley <smcinnism@gmail.com>
2025-02-11 14:58:19 -0800

    Add files via upload
    Computer Inventory Script. Gather list of IPs from CSV. Check if IP is reachable, Check if resolves through DNS, and Check Department.

---
+++ Inventory.ps1
@@ -0,0 +1,27 @@
+﻿$rows = Import-Csv -path C:\Users\Administrator\Documents\IPAddresses.csv
+
+foreach($row in $rows) {
+try {
+$output = @{
+IpAddress = $row.IPAddress
+Department = $row.Department
+IsOnline = $false
+HostName = $null
+Error = $null
+} 
+
+if (Test-connection -ComputerName $row.IPAddress -Count 1 -Quiet) {
+$output.IsOnline = $true
+}
+
+if ($hostname = (Resolve-DnsName -Name $row.IPAddress -ErrorAction Stop).Name) {
+$output.Hostname = $hostName
+}
+} catch {
+$output.Error = $_.Exception.Message
+} finally {
+[pscustomobject]$output
+}
+
+}
+

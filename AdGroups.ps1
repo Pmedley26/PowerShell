e6489ddae38b8a13f7a6bf28561f6f6c3e2d19d5
Patrick Medley <smcinnism@gmail.com>
2025-02-18 12:41:39 -0500

    Add files via upload

---
+++ AdGroups.ps1
@@ -0,0 +1,14 @@
+﻿$User = Read-Host -Prompt "Enter Username"
+$groups = "Legal Employees", "O365 Licensed Employees", "MTech All"
+
+foreach ($group in $groups) {
+try {
+Add-ADGroupMember -Identity $group -Members $User -Erroraction stop
+Write-Host "User '$user' added to group '$group' successfully."
+} catch {
+Write-Warning "Failed to add user '$User' to group '$group'. Error: $($_.Exception.Message)"
+
+}
+
+
+} 

99c172d7fd7c6be3d32c26a74a23e8b9a60e6e8f
Patrick Medley <smcinnism@gmail.com>
2026-02-27 14:24:16 -0500

    Add files via upload

---
+++ Check Server Stats.ps1
@@ -0,0 +1,13 @@
+﻿$files = get-childitem .\ -Recurse -Filter '*.py'
+
+#$files | Get-Member
+
+$files | Copy-Item
+
+if(!(Test-Path -path "C:\Users\pmedley\Desktop\demo-folder")) {
+New-Item -Itemtype "directory" -Path "C:\Users\pmedley\Desktop\demo-folder"
+}
+
+Foreach($file in $files){
+Copy-Item -path $file.Fullname -Destination "C:\Users\pmedley\Desktop\demo-folder"
+}

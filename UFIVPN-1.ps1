7d8041b0bd1d7f12502c03f71f05d2f13796617f
Patrick Medley <smcinnism@gmail.com>
2026-04-15 22:13:55 -0400

    Delete UFIVPN-1.ps1

--- UFIVPN-1.ps1
+++
@@ -1,5 +0,0 @@
-﻿$ServerAddress = "austell-ga-nkvrqwkjpj.dynamic-m.com"
-$ConnectionName = "UFI VPN"
-$PresharedKey = "MerakiUFI"
-
-Add-VpnConnection -Name "$ConnectionName" -ServerAddress "$ServerAddress" -TunnelType L2tp -L2tpPsk "$PresharedKey" -AuthenticationMethod Pap -Force

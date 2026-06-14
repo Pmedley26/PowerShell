7cc8e95e11625a26b0ddb4724ea4c5dce3f66d8c
Patrick Medley <smcinnism@gmail.com>
2026-06-13 22:58:12 -0400

    Add files via upload
    Collects AD accounts that haven't been logged into in 30 days

---
+++ Last-Login-30Days.ps1
@@ -0,0 +1,16 @@
+﻿Import-Module ActiveDirectory
+
+$DaysInactive = 30
+$CutoffDate = (Get-Date).AddDays(-$DaysInactive)
+
+$InactiveUsers = Get-ADUser -Filter * -Properties LastLogonDate, Enabled |
+    Where-Object {
+        $_.Enabled -eq $true -and
+        (
+            $_.LastLogonDate -lt $CutoffDate -or
+            $_.LastLogonDate -eq $null
+        )
+    } |
+    Select-Object Name, SamAccountName, Enabled, LastLogonDate
+
+$InactiveUsers
